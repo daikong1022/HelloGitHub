9e241944ebae4fd5aedd9df1b9db9754e9413fcd
佐藤潤 <satoujun@satoujunnoMacBook-Pro.local>
2019-10-19 16:30:05 +0900

    first commit

---
+++ HelloGithub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloGithub
+//
+//  Created by 佐藤潤 on 2019/10/19.
+//  Copyright © 2019 DaichiSato.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
